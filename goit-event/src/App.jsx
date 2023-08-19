dc2e0ba4f509235bf9a5093b9bdd7a5011d4b78e
Doniben <doniben@esperanto.co>
2023-08-19 14:24:02 -0600

    basic app with counter

--- goit-event/src/App.jsx
+++ goit-event/src/App.jsx
@@ -3,10 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import goitLogo from './assets/Goit-logo.png'
 import './App.css'
-import ListRenderer from './components/ListRenderer'
-import { FunctionalListRenderer } from './components/FunctionalListRenderer'
 
 function App() {
+  const [ counter, setCounter ] = useState(0)
+
+  const sumar = () => {
+    setCounter(counter + 1)
+  }
+
+  const restar = () => {
+    setCounter(counter - 1)
+  }
+
+  const reset = () => {
+    setCounter(0)
+  }
 
   return (
     <div className="App">
@@ -22,8 +33,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + GOIT</h1>
-      <ListRenderer />
-      <FunctionalListRenderer />
+      <h2 className='text-white'>{counter}</h2>
+      <div className="d-flex justify-content-center">
+        <button onClick={sumar} className="btn btn-success mr-2">+1</button>
+        {counter > 0 && <button onClick={restar} className="btn btn-warning mr-2">-1</button>}
+        <button onClick={reset} className="btn border btn-light text-withe">reset</button>
+      </div>
     </div>
   )
 }
