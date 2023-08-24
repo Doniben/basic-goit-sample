9ae8d933027cc41b245d7f88712617f3154bff4b
Doniben <doniben@esperanto.co>
2023-08-23 20:09:34 -0600

    useSearch params example

--- goit-event/src/5-Enrutamiento/pages/LoginPage.jsx
+++ goit-event/src/5-Enrutamiento/pages/LoginPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const LoginPage = () => {
+  const [ searchParams ] = useSearchParams()
+  const email = searchParams.get('email')
+  const password = searchParams.get('password')
+  const role = searchParams.get("role")
   return (
     <div>
-      LoginPage
+      <h2>LoginPage</h2>
+      <p>email: { email }</p>
+      <p>password: { password }</p>
+      <p>role: { role }</p>
     </div>
   )
 }
