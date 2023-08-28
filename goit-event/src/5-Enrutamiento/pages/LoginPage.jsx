31e51b0de4fe8bfe3ec0d23cad61867bebb8cce7
Doniben <doniben@esperanto.co>
2023-08-28 09:10:37 -0600

    Basic user context

--- goit-event/src/5-Enrutamiento/pages/LoginPage.jsx
+++ goit-event/src/5-Enrutamiento/pages/LoginPage.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { UserContext } from '../../context/UserContext'
+
 
 const LoginPage = () => {
   const [ searchParams ] = useSearchParams()
+  const userContextData = useContext(UserContext)
+  console.log(userContextData)
   const email = searchParams.get('email')
   const password = searchParams.get('password')
   const role = searchParams.get("role")
@@ -12,6 +16,10 @@ const LoginPage = () => {
       <p>email: { email }</p>
       <p>password: { password }</p>
       <p>role: { role }</p>
+      <hr />
+      <p>contextname: { userContextData.name } </p>
+      <p>contextid: { userContextData.id } </p>
+      <p>contextrole: { userContextData.role }  </p>
     </div>
   )
 }
--- goit-event/src/main.jsx
+++ goit-event/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 // import App from './App'
 import './index.css'
 import RouteApp from './5-Enrutamiento/RouteApp'
+import { UserProvider } from './context/UserProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter> {/* Componente de nivel superior(Higher order component) */}
-      <RouteApp />
+      <UserProvider>
+        <RouteApp />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
